3fb74ca09c11b3f31e6797b16534d7ccaf16c95c
Jan-Paul Hoga <janpaulhoga@Jan-Pauls-MacBook-Pro.local>
2020-10-27 00:46:19 +0900

    React Boiler Plate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { format } from "prettier";
 import Pet from "./Pet";
 
+
 const App = () => {
-  return React.createElement("div", { id: "someting-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Peter",
-      animal: "Cat",
-      breed: "something",
-    }),
-    React.createElement(Pet, {
-      name: "Peter",
-      animal: "Cat",
-      breed: "something",
-    }),
-    React.createElement(Pet, {
-      name: "Peter",
-      animal: "Cat",
-      breed: "something",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Peter" animal="Cat" breed="Mixed" />
+    </div>
+  )
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
