c888ab31e30610f0d8d7391efd101fbc8faf0931
Jan-Paul Hoga <janpaulhoga@Jan-Pauls-MacBook-Pro.local>
2020-11-16 22:58:01 +0900

    until Class Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Link, Router } from '@reach/router';
 import { format } from "prettier";
 import Pet from "./Pet";
 import SearchParams from "./SearchParams.js";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams /> 
+      <header>
+        <Link to="/">
+          Adopt Me!
+        </Link>
+      </header>
+      <Router>
+        <SearchParams path="/" /> 
+        <Details path="/details/:id" />
+      </Router>
     </div>
   )
 };
